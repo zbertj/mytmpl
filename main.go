b8bc67f28e15bb320ea68196ff07d3fb62cf2b52
zbertj <zbertj@163.com>
2020-07-25 15:23:33 +0800

    git push

--- main.go
+++ main.go
@@ -1,16 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"mytmpl/utils/compress"
-)
-
 func main() {
-	fmt.Println("--------------running-----------------")
-	soruce := []string{"./fortest/1.txt"}
-	err := compress.Zip(soruce, "fortest/1.zip")
-	if err != nil {
-		fmt.Println(err)
-	}
-	compress.Unzip("fortest/1.zip", "fortest")
+
 }
